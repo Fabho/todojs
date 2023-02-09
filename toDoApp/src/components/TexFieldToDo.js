653ef32715a56b81996adbfa5ea5499b5a2dd7e2
Jesus Vasquez <jesusvasquezesc7@gmail.com>
2023-02-09 19:43:44 -0400

    app se corrije el estado

--- toDoApp/src/components/TexFieldToDo.js
+++ toDoApp/src/components/TexFieldToDo.js
@@ -4,14 +4,15 @@ import TextField from '@mui/material/TextField';
 import { useState } from "react";
 import Stack from '@mui/material/Stack';
 function TexFieldToDo(){
-    function warning(){
-        alert("Presionaste el boton");
-    }
     const[text,setText]=useState(" ");
     const write=()=>{
         setText(...text);
         console.log(text);
     };
+    function warning(){
+        alert("Presionaste el boton");
+    }
+    
     
     return(
         <div>
