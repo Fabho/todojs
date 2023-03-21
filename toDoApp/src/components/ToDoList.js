132ae7c295a567da6663f8069c802bf086a6da59
Jesus Vasquez <jesusvasquezesc7@gmail.com>
2023-03-21 10:00:47 -0400

    3

--- toDoApp/src/components/ToDoList.js
+++ toDoApp/src/components/ToDoList.js
@@ -1,6 +1,5 @@
 import Button from '@mui/material/Button';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItemText';
+
 
 import DeleteIcon from '@mui/icons-material/Delete';
 import IconButton from '@mui/material/IconButton';
@@ -8,17 +7,17 @@ import EditIcon from '@mui/icons-material/Edit';
 function ToDoList(props) {
   return (
    <div>
-      <List>
+      <ul>
         {props.toDos.map((dats) => (
-        <ListItem key={dats.id}  style={{display:'flex', justifyContent:'flex-start'}}>
-         
+        <div key={dats.id}  style={{display:'flex', justifyContent:'space-between',alignItems: 'center'}}>
+         <div>
           {
             props.toEdit===dats.id ?
               (<input type="text" onChange={(e)=>props.textEdit(e.target.value)} defaultValue={dats.content}/>) : 
-              (<val style={{marginRight: '80px'}} >{dats.content}</val>)
+              (<val style={{alignItems: "center",marginRight: '90px'}} >{dats.content}</val>)
           } 
-         
-         
+         </div>
+         <div>
         <IconButton onClick={()=>{props.setter(props.toDos.filter(a=> a.id!==dats.id))}}  >
           <DeleteIcon  />
         </IconButton>
@@ -28,10 +27,11 @@ function ToDoList(props) {
             ( <><Button onClick={()=>props.editTo(dats.id)}>Submit</Button> <Button onClick={props.cancel}>Cancel</Button></>):
             (<IconButton onClick={()=>props.edit(dats.id)}><EditIcon /></IconButton>)
         }
-        </ListItem>
+        </div>
+        </div>
          ))}
         
-      </List>
+      </ul>
       
    </div>
   );
